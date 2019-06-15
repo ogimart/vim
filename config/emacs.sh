6432e686a5d06da0b3afbca398b34de0613ed568
Ogi Martinovic <ogimart@gmail.com>
2019-06-15 16:46:46 -0400

    fix link to alias

--- config/emacs.sh
+++ config/emacs.sh
@@ -4,5 +4,5 @@ DIR=`pwd`
 mkdir -p ~/.emacs.d
 mkdir -p ~/.emacs.d/eshell
 cd ~
-ln -s $DIR/init.el ~/.emacs.d/init.el
-ln -s $DIR/eshell/alias ~/.emacs.d/eshell/alias
+ln -s $DIR/.emacs.d/init.el ~/.emacs.d/init.el
+ln -s $DIR/.emacs.d/alias ~/.emacs.d/eshell/alias
